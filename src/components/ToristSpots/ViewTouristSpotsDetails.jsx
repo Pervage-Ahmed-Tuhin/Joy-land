f9f0861738594ac72e2f748e13d619f0423daf59
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-04-29 19:07:50 +0600

    fixed some bugs

--- src/components/ToristSpots/ViewTouristSpotsDetails.jsx
+++ src/components/ToristSpots/ViewTouristSpotsDetails.jsx
@@ -69,7 +69,7 @@ const ViewTouristSpotsDetails = () => {
                     <div className="hero-content flex-col lg:flex-row gap-8">
                         <img src={image} className="max-w-xs lg:max-w-xl rounded-lg shadow-2xl" />
                         <div>
-                            <h1 className="text-2xl lg:text-5xl font-bold">Spot Name :
+                            <h1 className="text-2xl lg:text-5xl font-bold text-gray-800">Spot Name :
 
                                 <Typewriter
                                     loop
@@ -86,20 +86,20 @@ const ViewTouristSpotsDetails = () => {
                             </h1>
                             <div className="divider"></div>
                             <div className="space-y-6">
-                                <h1 className="font-play-fare text-2xl font-bold ">Description: <br /></h1>
+                                <h1 className="font-play-fare text-2xl font-bold text-gray-800 ">Description: <br /></h1>
                                 <p className="py-6 font-play-fare text-gray-600 text-xl ">{textarea}</p>
 
-                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl font-bold">Country Name:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{countryName}</span></h1>
+                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl font-bold text-gray-800">Country Name:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{countryName}</span></h1>
 
-                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold">Location:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{location}</span></h1>
+                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold text-gray-800">Location:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{location}</span></h1>
 
-                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold">Average Coast:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{averageCost}$</span></h1>
+                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold text-gray-800">Average Coast:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{averageCost}$</span></h1>
 
-                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold">Seasonality:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{seasonality}✨</span></h1>
+                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold text-gray-800">Seasonality:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{seasonality}✨</span></h1>
 
-                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold">Traveling Time:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{travelTime} days ✈</span></h1>
+                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold text-gray-800">Traveling Time:</span> <span className="text-gray-600 text-xl lg:text-2xl ">{travelTime} days ✈</span></h1>
 
-                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold">Total Visitors Per Year:</span> <span className="text-gray-500 text-xl lg:text-2xl ">{totalVisitorsPerYear}    👨🏿‍🤝‍👨🏿👨🏿‍🤝‍👨🏿</span></h1>
+                                <h1 className="font-play-fare"><span className="text-xl lg:text-2xl  font-bold text-gray-800">Total Visitors Per Year:</span> <span className="text-gray-500 text-xl lg:text-2xl ">{totalVisitorsPerYear}    👨🏿‍🤝‍👨🏿👨🏿‍🤝‍👨🏿</span></h1>
                             </div>
 
 
