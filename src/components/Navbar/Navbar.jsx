92857e13e4a8e3da05e3f81ae517b91005b20eb2
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-04-29 10:52:10 +0600

    Fixed the toggle button issue

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -101,20 +101,24 @@ const Navbar = () => {
                             {links}
 
 
-                            <label htmlFor="Toggle2" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-800">
+                            <li>
 
-                                <span className="text-sm md:text-2xl text-[#00BFA6]"><IoMdSunny /></span>
-                                <span className="relative">
-                                    <input
 
-                                        onChange={handleToggle}
+                                <label htmlFor="Toggle1" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-800">
+                                    <span className="text-[#00BFA6]"><IoMdSunny /></span>
+                                    <span className="relative">
+                                        <input
 
-                                        id="Toggle2" type="checkbox" className="hidden peer" />
-                                    <div className="w-10 h-4 rounded-full shadow dark:bg-gray-400 peer-checked:dark:bg-gray-400"></div>
-                                    <div className="absolute left-0 w-6 h-6 rounded-full shadow -inset-y-1 peer-checked:right-0 peer-checked:left-auto dark:bg-[#00BFA6]"></div>
-                                </span>
-                                <span className="text-sm md:text-2xl text-[#00BFA6] mr-4"><BsMoonStarsFill /></span>
-                            </label>
+                                            onChange={handleToggle}
+
+                                            id="Toggle1" type="checkbox" className="hidden peer" />
+                                        <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-600 peer-checked:dark:bg-[#00BFA6]"></div>
+                                        <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-100"></div>
+                                    </span>
+                                    <span className="text-[#00BFA6]"><BsMoonStarsFill /></span>
+                                </label>
+
+                            </li>
 
                         </ul>
                     </div>
@@ -140,16 +144,7 @@ const Navbar = () => {
 
                 </div>
 
-                <div className="ml-0 md:ml-3 hidden md:block">
-                    {/* <label className="cursor-pointer grid place-items-center text-[#00BFA6]">
-                        <input
-
-                            onChange={handleToggle}
-
-                            type="checkbox" value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2 focus:ring-[#00BFA6] appearance-none border-none rounded-md" />
-                        <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
-                        <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-                    </label> */}
+                {/* <div className="ml-0 md:ml-3 hidden md:block">
 
                     <label htmlFor="Toggle2" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-800">
                         <span className="text-sm md:text-2xl text-[#00BFA6]"><IoMdSunny /></span>
@@ -168,21 +163,37 @@ const Navbar = () => {
 
 
 
+                </div> */}
+
+                <div className="ml-0 md:ml-3 hidden md:block">
+                    <label htmlFor="Toggle2" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-800">
+                        <span className="text-sm md:text-2xl text-[#00BFA6]"><IoMdSunny /></span>
+                        <span className="relative">
+                            <input
+                                onChange={handleToggle}
+                                id="Toggle2"
+                                type="checkbox"
+                                className="hidden peer" />
+                            <div className="w-10 h-4 rounded-full shadow dark:bg-gray-400 peer-checked:dark:bg-gray-400"></div>
+                            <div className="absolute left-0 w-6 h-6 rounded-full shadow -inset-y-1 peer-checked:right-0 peer-checked:left-auto dark:bg-[#00BFA6]"></div>
+                        </span>
+                        <span className="text-sm md:text-2xl text-[#00BFA6]"><BsMoonStarsFill /></span>
+                    </label>
                 </div>
 
                 <div className="navbar-end">
 
 
 
-                    
+
 
                     {user ? (
-                        <div className="flex ml-4 md:ml-0 items-center gap-5">
+                        <div className="flex  items-center gap-5">
                             <img
                                 title={user.displayName}
                                 className="rounded-full w-[20%] ml-3"
                                 src={user.photoURL}
-                                
+
                                 alt="You"
                             />
                             <button onClick={handleLogout} className="btn bg-[#00BFA6] text-white">
